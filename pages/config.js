f975abb1fa1a73bb1b00e3adcb4dc1d9243a472c
Austin Songer <austinvernsonger@protonmail.com>
2019-06-19 22:09:45 +0000

    Initial

---
+++ pages/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "title": "Prevail Health Solutions Platform",
+  "name": "prevail_health_solutions_platform",
+  "description": "Status Dashboard",
+  "defaultLocale": "en",
+  "locales": [
+    {
+      "code": "en",
+      "iso": "en-US",
+      "name": "English",
+      "file": "en.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "yaml"
+  }
+}
---
+++ pages/content/2019-06-19_regular-friday-deployment-hotfix.md
@@ -0,0 +1,15 @@
+---
+title: Regular Friday Deployment - Hotfix
+date: '2019-06-19T22:02:11.038Z'
+scheduled: '2019-06-28T17:00:00.895Z'
+severity: under-maintenance
+duration: 180
+affectedsystems:
+  - api
+  - dns
+  - site-delivery
+resolved: false
+---
+This is part of regular deplotment schedule.
+
+<!--- language code: en -->
