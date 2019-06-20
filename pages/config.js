42af50f3825ab56c419705cd4f29e285f8a241a9
Austin Songer <austinsonger@prevailhealth.com>
2019-06-20 09:39:29 -0500

    Update

---
+++ pages/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "title": "Prevail Health Solutions Platform",
+  "name": "prevail_health_solutions_platform",
+  "description": "Status Dashboard",
+  "defaultLocale": "en",
+  "locales": [
+    {
+      code: "en",
+      iso: "en-US",
+      name: "English",
+      file: "en.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "yaml",
+ /**     systems: [
+      "cdn",
+      "dns",
+      "helpdesk",
+      "site",
+      "api"
+      ]
+      **/
+}
+}
